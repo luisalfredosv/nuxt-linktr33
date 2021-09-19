acb38078df164621b7127fcf83e958472a5b3a65
luisalfredosv <luissarabia80@gmail.com>
2021-09-19 13:36:57 -0400

    [CREATE] default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Navbar />
+      <div>
+        <Nuxt />
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+     
+    }
+  },
+}
+</script>
